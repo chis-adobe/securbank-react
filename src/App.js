9b5c7444e3f3e07bca67853b3a75998f1d58c55a
Zoran Nikolovski <nikolovs@adobe.com>
2024-08-27 15:35:07 +1000

    Adding React Helmet for dynamic header values

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import FetchContent from './api/contentrequest';
 import FAQ from './components/faq';
 import Offer from './components/offer';
 
+import { Helmet } from 'react-helmet-async';
+
 function App() {
   const [content, setContent] = useState(null);
   const [submittedEmail, setSubmittedEmail] = useState('');
@@ -35,6 +37,9 @@ function App() {
             
   return (
     <div className="App">
+      <Helmet>
+        <meta name="urn:adobe:aue:system:aemconnection" content="aem:${process.env.REACT_APP_AEM_AUTHOR}"></meta>
+      </Helmet>
       <header className="App-header">
         <div className='header-alerts'>
             <p><strong>Alert!</strong> Scams are growing ever more complex and sophisticated. Learn more about protecting yourself from scams</p>
