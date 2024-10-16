0b4f304503f91fd88b6583724a10bc77c35c7265
Liviu Chis <chis@adobe.com>
2024-10-16 13:57:20 -0700

    Updated article

--- src/components/articles.js
+++ src/components/articles.js
@@ -9,7 +9,7 @@ function Articles({articles}) {
         <ul className="articleList">
             {articles && articles.map((article, index) => (
                 <li key={index} data-aue-resource={"urn:aemconnection:" + article._path + "/jcr:content/data/master"} data-aue-type="reference" data-aue-filter="cf">
-                    <img data-aue-prop="heroImage" data-aue-type="media" className="articleImage" alt="decorative" src={aempublishurl + article.heroImage[0]._dynamicUrl + "&width=470"} />
+                    <img data-aue-prop="heroImage" data-aue-type="media" className="articleImage" alt="decorative" src={aempublishurl + article.heroImage._dynamicUrl + "&width=470"} />
                     <h5 data-aue-prop="headline" data-aue-type="text" className="articleHeading">{article.headline}</h5>
                     <div data-aue-prop="main" data-aue-type="richtext" className="articleDescription">{article.main['plaintext']}</div>
                 </li>
